ec439c5dbf0b0d452b580679759dbb4cd110f088
Martin Dines <martin.dines@live.co.uk>
2019-08-20 23:10:55 +0100

    Initial Commit

---
+++ ScreenshotClient/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenshotClient
+//
+//  Created by Martin Dines on 20/08/2019.
+//  Copyright © 2019 Martin Dines. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScreenshotClient/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenshotClient
+//
+//  Created by Martin Dines on 20/08/2019.
+//  Copyright © 2019 Martin Dines. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
